239f15738babd5a523d3d15ded43ade0c64fd087
grit <136907108@qq.com>
2022-02-23 15:51:08 +0800

    chore: 更改eslint配置

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/*
+ * @Description: 输入文件描述信息
+ * @Author: liu-wb
+ * @Date: 2022-02-23 15:26:01
+ * @LastEditTime: 2022-02-23 15:49:26
+ */
 
 import Vue from 'vue'
 import 'normalize.css/normalize.css'
@@ -13,7 +19,7 @@ import './icons' // icon
 import mixins from '@/mixins'
 import ReadConfig from '@/utils/config';
 
-(async() => {
+(async () => {
   await ReadConfig(Vue)
   Vue.config.productionTip = false
   Vue.prototype.appConfig = Setting
